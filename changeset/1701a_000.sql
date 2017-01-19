229eecc6b868f3f0f1819c5b11db0a0597e16418
amcdowell <armcdowell@hotmail.com>
2017-01-20 10:16:45 +1300

    removed changesets from SOLA-FAO

---
+++ changeset/1701a_000.sql
@@ -0,0 +1,7 @@
+﻿-- 20 Jan 2017
+-- Example changeset to create for database changes.
+
+-- Add changeset to system.version table so its possible to determine if hte chagneset has been run. 
+INSERT INTO system.version SELECT '1701a' WHERE NOT EXISTS (SELECT version_num FROM system.version WHERE version_num = '1701a');
+
+-- Changeset SQL to go below. Provide comments for the SQL statements to clarify their purpose. 
